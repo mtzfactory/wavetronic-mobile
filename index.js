adc2c96a1a3a3cd9b07afd52908ec00ee3dfb2d7
Ricardo Martinez <ricardo.martinez.monje@gmail.com>
2017-11-16 17:23:00 +0100

    Add .gitignore folders ios and android.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './app/App';
 
-AppRegistry.registerComponent('music_share', () => App);
+AppRegistry.registerComponent('waveMyBeat', () => App);
